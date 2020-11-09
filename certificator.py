fab3fe0104ee6a44416f40c79e0d71310efb1d42
Rodrigo Milan Calegari <42324712+RodrigoMilan@users.noreply.github.com>
2020-11-08 22:48:01 -0300

    Create certificator.py
    I need to improve a fell things:
    Only working for gmail users, for now.
    I need to add a option to personalize the message.

---
+++ certificator.py
@@ -0,0 +1,35 @@
+from PIL import Image, ImageDraw, ImageFont
+from email.message import EmailMessage
+import pandas as pd
+import smtplib
+import imghdr
+
+def main(gmail_user, gmail_pass, excel, certificate):
+
+	df = pd.read_excel(excel)
+
+	name_list = df['Name'].to_list()
+	email_list = df['email'].to_list()
+
+	for i in range(len(name_list)):
+		im = Image.open(certificate)
+		d = ImageDraw.Draw(im)
+		location = (72,280)
+		text_color = (0,0,0)
+		font = ImageFont.truetype("Arial.TTF", 20)
+		d.text(location,name_list[i],fill=text_color,font=font)
+		im.save("certificate_"+name_list[i]+".pdf")
+
+		msg = EmailMessage()
+		msg['Subject'] = 'Certificate'
+		msg['From'] = gmail_user
+		msg['To'] = email_list[i]
+		with open("certificate_"+name_list[i]+".pdf",'rb') as fp_file:
+			fp = fp_file.read()
+		msg.add_attachment(fp, maintype='application',subtype='pdf',filename='certificate.pdf')
+
+		server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+		server.ehlo()
+		server.login(gmail_user,gmail_pass)
+		server.send_message(msg)
+		server.close()
